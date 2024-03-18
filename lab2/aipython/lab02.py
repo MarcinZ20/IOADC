29caeefe7b44f0abf00ca58a886043ac077eab11
kubijaku <kubijaku11@gmail.com>
2024-03-18 09:27:53 +0100

    custom heuristic

--- lab2/aipython/lab02.py
+++ lab2/aipython/lab02.py
@@ -4,6 +4,19 @@ from stripsProblem import *
 from searchBranchAndBound import DF_branch_and_bound
 from searchMPP import SearcherMPP
 import stripsProblem
+import time
+
+
+def custom_heur_1(state, goal):
+ heur_value = 0
+ for s, v in state.items():
+  for s_g, v_g in goal.items():
+   if s == s_g:
+    heur_value += not v == v_g
+ # print("State:", state)
+ # print("Goal:", goal)
+ # print("Heurisitc:", heur_value)
+ return heur_value
 
 blocks1domain = create_blocks_world({'a', 'b', 'c', 'd', 'e', 'f'})
 blocks2domain = create_blocks_world({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'})
@@ -31,7 +44,7 @@ problem2 = Planning_problem(blocks1domain,
                              clear('d'): True, on('d'): 'e',
                              clear('e'): False, on('e'): 'f'})  # goal
 
-problem3 = Planning_problem(blocks1domain,
+problem3 = Planning_problem(blocks2domain,
                             {on('a'): 'table', clear('a'): True,
                              on('b'): 'table', clear('b'): True,
                              on('c'): 'table', clear('c'): True,
@@ -42,13 +55,14 @@ problem3 = Planning_problem(blocks1domain,
                              on('h'): 'table', clear('h'): True,
                              on('i'): 'table', clear('i'): True},  # initial state
                             {clear('a'): True, on('a'): 'table',
-                             clear('b'): False, on('b'): 'c',
-                             clear('c'):True, on('c'): 'table',
+                             clear('b'): False, on('b'): 'table',
+                             clear('c'): True, on('c'): 'b',
                              clear('d'): False, on('d'): 'table',
                              clear('e'): False, on('e'): 'd',
                              clear('f'): True, on('f'): 'e',
-                             clear('g'): True, on('g'): 'table',
-                             clear('h'): True, on('h'): 'table'})  # goal
+                             clear('g'): False, on('g'): 'table',
+                             clear('h'): True, on('h'): 'g',
+                             clear('i'): True, on('i'): 'table'})  # goal
 
 print("Length:", len(problem1.initial_state))
 #
@@ -63,11 +77,34 @@ from searchBranchAndBound import DF_branch_and_bound
 from searchMPP import SearcherMPP
 import stripsProblem
 
+print("\n\nwith custom heuristic\n\n")
+timeStart = time.time()
+
+SearcherMPP(Regression_STRIPS(problem1, heur=custom_heur_1)).search()   #A* with MPP
+# DF_branch_and_bound(Regression_STRIPS(problem1),10).search() #B&B
+#
+SearcherMPP(Regression_STRIPS(problem2, heur=custom_heur_1)).search()   #A* with MPP
+# DF_branch_and_bound(Regression_STRIPS(problem2),10).search() #B&B
+#
+SearcherMPP(Regression_STRIPS(problem3, heur=custom_heur_1)).search()   #A* with MPP
+# DF_branch_and_bound(Regression_STRIPS(problem3),10).search() #B&B
+
+timeStop = time.time()
+
+print("time: ", timeStop - timeStart)
+
+
+
+print("\n\nwithout custom heuristic\n\n")
+timeStart = time.time()
 SearcherMPP(Regression_STRIPS(problem1)).search()   #A* with MPP
-DF_branch_and_bound(Regression_STRIPS(problem1),10).search() #B&B
-
+# DF_branch_and_bound(Regression_STRIPS(problem1),10).search() #B&B
+#
 SearcherMPP(Regression_STRIPS(problem2)).search()   #A* with MPP
-DF_branch_and_bound(Regression_STRIPS(problem2),10).search() #B&B
-
+# DF_branch_and_bound(Regression_STRIPS(problem2),10).search() #B&B
+#
 SearcherMPP(Regression_STRIPS(problem3)).search()   #A* with MPP
-DF_branch_and_bound(Regression_STRIPS(problem3),10).search() #B&B
+# DF_branch_and_bound(Regression_STRIPS(problem3),10).search() #B&B
+timeStop = time.time()
+print("time: ", timeStop - timeStart)
+
