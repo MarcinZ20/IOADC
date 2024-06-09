a495d247006ed5460fbf87ccfd32140d4c4ea298
mzub <marcin_zub@outlook.com>
2024-06-09 15:18:04 +0200

    Notebook

--- lab6/main.py
+++ lab6/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import supersuit as ss
 from pettingzoo.atari import pong_v3
 
-env = pong_v3.env(num_players=2, max_cycles=1000, render_mode='human', auto_rom_install_path=Path("~/.virtualenvs/ioadc/lib/python3.10/site-packages/AutoROM/roms/pong.bin"))
+env = pong_v3.env(num_players=2, max_cycles=1000, render_mode='human', auto_rom_install_path=Path("Users/marcin/.virtualenvs/ioadc/lib/python3.10/site-packages/AutoROM/roms"))
 env = ss.max_observation_v0(env, 2)
 env = ss.sticky_actions_v0(env, repeat_action_probability=0.25)
 env = ss.frame_skip_v0(env, 4)
